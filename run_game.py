f94bf958f7e6c7b580b35072cb57bd667e688417
Alexander Izmailov <yarolig@gmail.com>
2018-04-21 17:38:19 +0300

    pyinstaller workaround

--- run_game.py
+++ run_game.py
@@ -1,6 +1,22 @@
 #! /usr/bin/env python
-import cProfile
+
+if 0:
+    # PyInstaller workaround
+    # https://github.com/pyinstaller/pyinstaller/issues/2820
+    import UserList
+    import UserString
+    import UserDict
+    import itertools
+    import collections
+    import future.backports.misc
+    import commands
+    import base64
+    import __buildin__
+    import math
+    import reprlib
+    import functools
+    import re
+    import subprocess
 
 from gamelib import main
 main.main()
-#cProfile.run('''main.main()''')
